0dfb056a258c51684cc5366e8ce5130d838ae75d
TUSHAR_GIT <er.tushar2014@gmail.com>
2020-07-21 18:33:28 +0530

    added changes for dir and port

--- src/main/java/com/example/ISOBridgeApplicationRunner.java
+++ src/main/java/com/example/ISOBridgeApplicationRunner.java
@@ -2,13 +2,32 @@ package com.example;
 
 import org.jpos.q2.Q2;
 
+import java.io.File;
+
 public class ISOBridgeApplicationRunner {
 
-	public static void main(String[] args) {
+	public static final String  Q2_LOG = "./log/";
+	public static final String  Q2_FILE = Q2_LOG+"q2.log";
+	public static final String  Q2_DEPlOY = "./deploy/";
+
+
+	public static void main(String[] args) throws Exception{
+
+		mkdirs(Q2_DEPlOY);
+		mkdirs(Q2_LOG);
+		File q2file = new File(Q2_FILE);
+		boolean unused =  q2file.createNewFile();
+
 		System.out.println("start");
 		Q2 q2 = new Q2();
 		q2.start();
 		System.out.println("end");
 	}
-	
+
+	private static void mkdirs(String dirPath) throws  Exception{
+		File dir = new File(dirPath);
+		if(!dir.mkdirs()) {
+			throw new Exception("Could not create directory "+dir.getAbsolutePath());
+		}
+	}
 }
